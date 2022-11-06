390c05c1ff069077f79ab9c1a784757b59954c32
Han Xiao <xiaohan2012@gmail.com>
2022-11-06 21:49:17 +0200

    .

--- config.el
+++ config.el
@@ -1,3 +1,9 @@
+(setenv "PATH" (concat ":/usr/local/bin/" (getenv "PATH")))
+(add-to-list 'exec-path "/usr/local/bin/")
+
+(setenv "PATH" (concat ":/usr/bin/" (getenv "PATH")))
+(add-to-list 'exec-path "/usr/bin/")
+
 (require 'package)
 
 
@@ -80,10 +86,32 @@
 (org-babel-do-load-languages
  'org-babel-load-languages '((python . t)))
 
+;; Making emacs find latex (so that C-c C-x C-l works on orgmode)
+;; On MacOS
+(setenv "PATH" (concat ":/Library/TeX/texbin/" (getenv "PATH")))
+(add-to-list 'exec-path "/Library/TeX/texbin/")
+
+(setq org-image-actual-width nil)
+(pixel-scroll-mode t) ;; enable pixel scroll mode for better image viewing
+
+(use-package org-journal
+  :ensure t
+  :defer t
+  :init
+  ;; Change default prefix key; needs to be set before loading org-journal
+  (setq org-journal-prefix-key "C-c j ")
+  :config
+  (setq org-journal-dir "~/org/journal/"
+	org-journal-date-format "%A, %d %B %Y"
+	org-journal-time-format "日记"))
+
 (use-package markdown-mode
   :ensure t
   :mode ("README\\.md\\'" . gfm-mode)
   :init (setq markdown-command "multimarkdown"))
+
+(custom-set-variables
+ '(markdown-command "/usr/local/bin/pandoc"))
 
 (defun my-put-file-name-on-clipboard ()
   "Put the current file name on the clipboard"
@@ -97,11 +125,100 @@
 	(clipboard-kill-region (point-min) (point-max)))
       (message filename))))
 
-(defun dmrs-org-visit ()
-"visit ~/docs/notes/dmrs.org"
+(defun dairy-org-visit ()
+"visit ~/docs/notes/dairy2022.org"
 (interactive)
-(find-file "~/docs/notes/dmrs.org"))
-(global-set-key (kbd "C-c o d") 'dmrs-org-visit)
+(find-file "~/docs/notes/dairy2022.org"))
+(global-set-key (kbd "C-c o d") 'dairy-org-visit)
+
+(use-package dired-subtree
+  :ensure t)
+
+(use-package dired-filter
+  :ensure t)
+
+(use-package neotree
+  :ensure t
+  :init
+  (setq neo-window-width 55)
+  )
+
+(defun my-open-file-at-cursor ()
+  "Open the file path under cursor.
+If there is text selection, uses the text selection for path.
+If the path starts with “http://”, open the URL in browser.
+Input path can be {relative, full path, URL}.
+Path may have a trailing “:‹n›” that indicates line number, or “:‹n›:‹m›” with line and column number. If so, jump to that line number.
+If path does not have a file extension, automatically try with “.el” for elisp files.
+This command is similar to `find-file-at-point' but without prompting for confirmation.
+
+URL `http://xahlee.info/emacs/emacs/emacs_open_file_path_fast.html'
+Version 2020-10-17"
+  (interactive)
+  (let* (
+	 ($inputStr
+	  (if (use-region-p)
+	      (buffer-substring-no-properties (region-beginning) (region-end))
+	    (let ($p0 $p1 $p2
+		      ;; chars that are likely to be delimiters of file path or url, e.g. whitespace, comma. The colon is a problem. cuz it's in url, but not in file name. Don't want to use just space as delimiter because path or url are often in brackets or quotes as in markdown or html
+		      ($pathStops "^  \t\n\"`'‘’“”|[]{}「」<>〔〕〈〉《》【】〖〗«»‹›❮❯❬❭〘〙·。\\"))
+	      (setq $p0 (point))
+	      (skip-chars-backward $pathStops)
+	      (setq $p1 (point))
+	      (goto-char $p0)
+	      (skip-chars-forward $pathStops)
+	      (setq $p2 (point))
+	      (goto-char $p0)
+	      (buffer-substring-no-properties $p1 $p2))))
+	 ($path
+	  (replace-regexp-in-string
+	   "^file:///" "/"
+	   (replace-regexp-in-string
+	    ":\\'" "" $inputStr))))
+    (if (string-match-p "\\`https?://" $path)
+	(if (fboundp 'xahsite-url-to-filepath)
+	    (let (($x (xahsite-url-to-filepath $path)))
+	      (if (string-match "^http" $x )
+		  (browse-url $x)
+		(find-file $x)))
+	  (progn (browse-url $path)))
+      (progn ; not starting “http://”
+	(if (string-match "#" $path )
+	    (let (
+		  ( $fpath (substring $path 0 (match-beginning 0)))
+		  ( $fractPart (substring $path (1+ (match-beginning 0)))))
+	      (if (file-exists-p $fpath)
+		  (progn
+		    (find-file $fpath)
+		    (goto-char (point-min))
+		    (search-forward $fractPart ))
+		(when (y-or-n-p (format "file no exist: 「%s」. Create?" $fpath))
+		  (find-file $fpath))))
+	  (if (string-match "^\\`\\(.+?\\):\\([0-9]+\\)\\(:[0-9]+\\)?\\'" $path)
+	      (let (
+		    ($fpath (match-string 1 $path))
+		    ($line-num (string-to-number (match-string 2 $path))))
+		(if (file-exists-p $fpath)
+		    (progn
+		      (find-file $fpath)
+		      (goto-char (point-min))
+		      (forward-line (1- $line-num)))
+		  (when (y-or-n-p (format "file no exist: 「%s」. Create?" $fpath))
+		    (find-file $fpath))))
+	    (if (file-exists-p $path)
+		(progn ; open f.ts instead of f.js
+		  (let (($ext (file-name-extension $path))
+			($fnamecore (file-name-sans-extension $path)))
+		    (if (and (string-equal $ext "js")
+			     (file-exists-p (concat $fnamecore ".ts")))
+			(find-file (concat $fnamecore ".ts"))
+		      (find-file $path))))
+	      (if (file-exists-p (concat $path ".el"))
+		  (find-file (concat $path ".el"))
+		(when (y-or-n-p (format "file no exist: 「%s」. Create?" $path))
+		  (find-file $path ))))))))))
+
+(global-set-key (kbd "C-c o f") 'my-open-file-at-cursor)
 
 (use-package switch-window
 
@@ -247,6 +364,12 @@
 
 (use-package ein
   :ensure t
+  :custom
+  ;; use below to show inline images
+  ;; reference: https://github.com/peterewills/emacs-ipython-notebook
+  (ein:output-area-inlined-images t) ;; not necessary in older versions
+  (ein:slice-image t)
+  (pixel-scroll-mode t) ;; enable pixel scroll mode for better image viewing
   )
 
 (use-package elpy
@@ -422,6 +545,128 @@
 (use-package hungry-delete
 :ensure t
 :config (global-hungry-delete-mode))
+
+;; (global-set-key (kbd "C-c d p") 'delete-pair)
+
+(defun copy-chunk-at-point ()
+  "copy a path at point if it exists."
+  (interactive)
+  (let* (
+	 ($inputStr
+	  (if (use-region-p)
+	      (buffer-substring-no-properties (region-beginning) (region-end))
+	    (let ($p0 $p1 $p2
+		      ;; chars that are likely to be delimiters of file path or url, e.g. whitespace, comma. The colon is a problem. cuz it's in url, but not in file name. Don't want to use just space as delimiter because path or url are often in brackets or quotes as in markdown or html
+		      ($pathStops "^  \t\n\"`'‘’“”|[]{}「」<>〔〕〈〉《》【】〖〗«»‹›❮❯❬❭〘〙·。\\"))
+	      (setq $p0 (point))
+	      (skip-chars-backward $pathStops)
+	      (setq $p1 (point))
+	      (goto-char $p0)
+	      (skip-chars-forward $pathStops)
+	      (setq $p2 (point))
+	      (goto-char $p0)
+	      (buffer-substring-no-properties $p1 $p2))))
+	 ($path
+	  (replace-regexp-in-string
+	   "^file:///" "/"
+	   (replace-regexp-in-string
+	    ":\\'" "" $inputStr))))
+    (with-temp-buffer
+      (insert $inputStr)
+      (clipboard-kill-region (point-min) (point-max)))
+    (message (format "copied '%s'" $inputStr))
+    ))
+(global-set-key (kbd "C-c c c") 'copy-chunk-at-point)
+
+(defun surround-chunk-by-stuff ($stuff-to-insert)
+  "surround a text chunk by single quote"
+  ;; (interactive)
+  (if (use-region-p)
+      (buffer-substring-no-properties (region-beginning) (region-end))
+    (let ($curPos $startPos $endPos
+		  $startStuff $endStuff
+		  ;; chars that are likely to be delimiters of file path or url, e.g. whitespace, comma. The colon is a problem. cuz it's in url, but not in file name. Don't want to use just space as delimiter because path or url are often in brackets or quotes as in markdown or html
+		  ($pathStops "^  \t\n\"`'‘’“”|[]{}「」<>〔〕〈〉《》【】〖〗«»‹›❮❯❬❭〘〙·。\\")
+		  ;; ($stuff-to-insert (read-string "what string?"))
+		  )
+      ;; try to support different start and end chars, eg parenthesis and brackets
+      ;; however, it shows:
+      ;; if: Symbol’s value as variable is void: $sutff-to-insert
+      (setq $startStuff $stuff-to-insert)
+      (setq $endStuff $stuff-to-insert)
+      ;; (message "here! before if")	   
+      ;; (if (string= $sutff-to-insert "<")
+      ;;     (progn
+      ;; 	 (setq $startStuff "<")
+      ;; 	 (setq $endStuff ">"))
+      ;;   (if (string= $stuff-to-insert "(")
+      ;; 	 (progn
+      ;; 	   (setq $startStuff "(")
+      ;; 	   (setq $startStuff ")"))
+      ;;     (if (string= $stuff-to-insert "{")
+      ;; 	   (progn
+      ;; 	   (setq $startStuff "{")
+      ;; 	   (setq $startStuff "}"))
+      ;; 	 (if (string= $stuff-to-insert "[")
+      ;; 	   (progn
+      ;; 	   (setq $startStuff "[")
+      ;; 	   (setq $startStuff "]"))
+      ;; 	   (progn
+      ;; 	     (message "here!")
+      ;; 	     (setq $startStuff '$stuff-to-insert)
+      ;; 	     (setq $startStuff '$stuff-to-insert)))
+      ;; 	 ))
+      ;;     )	   
+      ;; save current position
+      (setq $curPos (point))
+      ;; get start position of the chunk
+      (skip-chars-backward $pathStops)
+      (insert $startStuff)
+      (setq $startPos (point))
+      ;; get stop position of the chunk
+      (goto-char $curPos)
+      (skip-chars-forward $pathStops)
+      (setq $endPos (point))
+      (insert $endStuff)
+      ;; back to original position
+      (goto-char $curPos)
+      )
+    )
+  )
+
+(defun surround-chunk-by-single-quote ()
+  (interactive)
+  (surround-chunk-by-stuff "'"))
+
+(defun surround-chunk-by-double-quote ()
+  (interactive)
+  (surround-chunk-by-stuff "\""))
+
+(defun surround-chunk-by-back-tick ()
+  (interactive)
+  (surround-chunk-by-stuff "`"))
+
+(defun surround-chunk-by-dollar ()
+  (interactive)
+  (surround-chunk-by-stuff "$"))
+
+
+
+(global-set-key (kbd "C-c s '") 'surround-chunk-by-single-quote)
+(global-set-key (kbd "C-c s \"") 'surround-chunk-by-double-quote)
+(global-set-key (kbd "C-c s $") 'surround-chunk-by-dollar)
+(global-set-key (kbd "C-c s `") 'surround-chunk-by-back-tick)
+
+(use-package smartparens-config
+  :ensure smartparens
+  :config (progn (show-smartparens-global-mode t)))
+
+(add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
+(add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
+
+(global-set-key (kbd "C-M-a") 'sp-beginning-of-sexp)
+(global-set-key (kbd "C-M-e") 'sp-end-of-sexp)
+;; (global-set-key (kbd "C-down") 'sp-down-sexp)
 
 (use-package spaceline
 :ensure t
