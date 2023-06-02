faa1dc7d1061b4f5651474887dc3a91cbd7a0a36
Han Xiao <xiaohan2012@gmail.com>
2023-06-02 09:03:39 +0800

    .

--- .mc-lists.el
+++ .mc-lists.el
@@ -7,15 +7,19 @@
 	beginning-of-buffer
 	comint-delchar-or-maybe-eof
 	electric-pair-delete-pair
+	emojify-delete-emoji-backward
 	end-of-buffer
 	forward-sexp
 	indent-for-tab-command
 	kill-region
+	lsp-signature-stop
 	mc/mark-next-lines
 	org-beginning-of-line
 	org-delete-char
 	org-edit-src-save
 	org-end-of-line
+	org-force-self-insert
+	org-kill-line
 	org-self-insert-command
 	sp-backward-delete-char
 	sp-backward-kill-word
@@ -23,6 +27,7 @@
 	sp-forward-sexp
 	sp-kill-region
 	sp-kill-word
+	yas-expand
 	))
 
 (setq mc/cmds-to-run-once
